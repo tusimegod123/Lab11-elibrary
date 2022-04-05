3a9ccd1e05a7801a10e29e8180d8d6d38d3cee07
Tusime godwin <tusimegodwinmarch2013@gmail.com>
2022-04-05 14:22:19 -0500

    Initial commit

---
+++ src/main/java/com/eLibrary/eLibrary/controller/HomePageController.java
@@ -0,0 +1,13 @@
+package com.eLibrary.eLibrary.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class HomePageController {
+    @GetMapping(value = {"/","/elibrary","/elibrary","/home"})
+    public String getHomePage(){
+        return "home/index";
+    }
+}
+
